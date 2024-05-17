5662bce6b55a47b1f58f806a8a5bfc7ef2f86445
Himanshu-Narula <himanshunarula20@gmail.com>
2024-05-17 10:38:50 +0530

    6 May 2024 WPP question's answer

--- WPP_day1ans.js
+++ WPP_day1ans.js
@@ -1,3 +1,12 @@
+// Question 1: Write a JavaScript program to display the current day and time in the following format.
+
+// Sample Output : 
+
+// Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
+
+// Answer 1:
+
 function getCurrentDayAndTime() {
     // Array of days
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
