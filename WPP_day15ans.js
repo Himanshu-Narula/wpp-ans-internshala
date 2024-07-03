39de409e69b5b3a3d5bc31db8fd29ba352903450
Himanshu-Narula <himanshunarula20@gmail.com>
2024-07-03 20:58:33 +0530

    2 July 2024 WPP question's answer

---
+++ WPP_day15ans.js
@@ -0,0 +1,11 @@
+// Question 15: Write a JavaScript program to compute the sum of three elements of a given array of integers of length 3.
+// Answer 15:
+
+function three_sum(nums) {
+    return nums[0] + nums[1] + nums[2];
+}
+
+// Example usecase:
+console.log(three_sum([23, 1, 2]));
+console.log(three_sum([55, 1, 99]));
+console.log(three_sum([102, 85, -110]));
